f0bedcfa33c488bc7c43c83e0a95d610a692c0b1
RobertHan96 <yshan4329@gmail.com>
2020-04-09 20:47:28 +0900

    3td commit : 전화걸기 인텐트 기능 구현

--- app/src/main/java/com/nexon/androidintent/MainActivity.kt
+++ app/src/main/java/com/nexon/androidintent/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.nexon.androidintent
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+    }
 
-        dialBtn
+    fun setupEvents() {
+        dialBtn.setOnClickListener {
+//      안드로이드 기본 인텐트를 쓸때는 Intent가 아닌 Uri를 이용해서 접근
+        val myUri = Uri.parse("tel:01022345619")
+        val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+        startActivity(myIntent)
+        }
     }
 }
