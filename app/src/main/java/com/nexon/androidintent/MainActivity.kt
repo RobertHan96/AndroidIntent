67508e3779e28c3c24f2f74fd5d18ba241cdd771
RobertHan96 <yshan4329@gmail.com>
2020-04-09 21:34:25 +0900

    6th commit : 문자, 웹페이지 이동, 플레이스토어 이동 인텐트 추가

--- app/src/main/java/com/nexon/androidintent/MainActivity.kt
+++ app/src/main/java/com/nexon/androidintent/MainActivity.kt
@@ -29,6 +29,28 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(Intent.ACTION_CALL, myUri)
             startActivity(myIntent)
         }
+
+        smsBtn.setOnClickListener {
+            val userNumber : String = editPhoneNumber.text.toString()
+            val myUri = Uri.parse("smsto:${userNumber}")
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+            myIntent.putExtra("sms_body", "문자 입력 내용")
+            startActivity(myIntent)
+        }
+
+        naverBtn.setOnClickListener {
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+
+        }
+
+        playStoreBtn.setOnClickListener {
+            val myUri = Uri.parse("https://play.google.com/store/apps/details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+
+        }
     }
 
 
