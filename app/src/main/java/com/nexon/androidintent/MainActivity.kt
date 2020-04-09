03a4c66133e621a71b028e8db61ad473cd68f3c1
RobertHan96 <yshan4329@gmail.com>
2020-04-09 20:52:48 +0900

    4th commit : 입력받은 번호로 전화거는 로직 추가

--- app/src/main/java/com/nexon/androidintent/MainActivity.kt
+++ app/src/main/java/com/nexon/androidintent/MainActivity.kt
@@ -16,8 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     fun setupEvents() {
         dialBtn.setOnClickListener {
+        val userNumber : String = editPhoneNumber.text.toString()
 //      안드로이드 기본 인텐트를 쓸때는 Intent가 아닌 Uri를 이용해서 접근
-        val myUri = Uri.parse("tel:01022345619")
+        val myUri = Uri.parse("tel:${userNumber}")
         val myIntent = Intent(Intent.ACTION_DIAL, myUri)
         startActivity(myIntent)
         }
